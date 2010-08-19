de46009ab59c5d6d0ffa3a8bf4814d6b278872b6
Erik Andersson <erik.rothoff@gmail.com>
2010-08-19 15:23:30 +0200

    Removed versions plugin
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- plugins/versions/revisions/1.sql
+++
@@ -1,1 +0,0 @@
--- Add you first version here.
--- plugins/versions/revisions/2.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `todoitem` ADD `list_id` INT( 11 ) NOT NULL AFTER `id`;
-
-CREATE TABLE `cowl`.`todolist` (
-`id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`name` INT( 75 ) NOT NULL
-) ENGINE = MYISAM;
