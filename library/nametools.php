37171f8c44b1e8e1349e282cb88b589e0a333257
Erik Andersson <erik.rothoff@gmail.com>
2010-08-20 10:22:00 +0200

    Changed Nametools to be multibyte compatible
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- library/nametools.php
+++ library/nametools.php
@@ -4,8 +4,8 @@ class NameTools
 {
 	public static function toSlug($name)
 	{
-		$name = strtolower($name);
-		$name = str_replace(array('å', 'ä', 'ö', 'Å', 'Ä', 'Ö'), array('a', 'a', 'o', 'a', 'a', 'o'), $name);
+		$name = mb_strtolower($name);
+		$name = str_replace(array('å', 'ä', 'ö'), array('a', 'a', 'o', 'a', 'a', 'o'), $name);
 		$name = preg_replace('/\W|\s/', '-', $name);
 		$name = preg_replace('/\-\-{1,}/', '-', $name);
 		$name = trim($name, '-');
