61770e8639e5b406218bc1919e938d8c40fdecc3
Erik Andersson <erik.rothoff@gmail.com>
2010-08-21 13:21:37 +0200

    Moved Plugin base class to plugins.php instead of its own file
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- plugins.php
+++ plugins.php
@@ -138,3 +138,28 @@ class Plugins
 		return str_replace(' ', '', ucwords(str_replace('_', ' ', $name)));
 	}
 }
+
+/*
+	Class:
+		<Plugin>
+	
+	Abstract base class for all plugins.
+*/
+
+abstract class Plugin
+{
+	// FrontController-related hooks
+	public function prePathParse(Controller $controller) {}
+	public function postPathParse($args) {}
+	public function postRun() {}
+	
+	// Command-related hooks
+	public function commandRun($method, $args) {}
+	
+	// ORM-related hooks
+	public function dbPopulate(DataMapper $mapper, DomainObject $object) {}
+	public function dbFind(DataMapper $mapper, $args) {}
+	public function dbInsert(DataMapper $mapper, DomainObject $object) {}
+	public function dbUpdate(DataMapper $mapper, DomainObject $object) {}
+	public function dbRemove(DataMapper $mapper, $id) {}
+}
