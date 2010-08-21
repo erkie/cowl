ae0cd318ad50ab8a7c375a476291c3099f13a4c4
Erik Andersson <erik.rothoff@gmail.com>
2010-08-21 22:41:02 +0200

    Fixed so the is_mandatory validator always returns true if is_mandatory is set to false
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- library/validators/validator.is_mandatory.php
+++ library/validators/validator.is_mandatory.php
@@ -1,6 +1,8 @@
 <?php
 
-function validate_is_mandatory($value)
+function validate_is_mandatory($value, $arg)
 {
+	// if is_mandatory is false everything is A-OK
+	if ( ! $arg ) return true;
 	return ! empty($value);
 }
