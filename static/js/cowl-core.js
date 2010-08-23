d43ceca0b7beb337744f9fb2448dfc6a5e002898
Erik Andersson <erik.rothoff@gmail.com>
2010-08-23 15:32:56 +0200

    Added templating system to JS cowl for easy integration with Templater
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- static/js/cowl-core.js
+++ static/js/cowl-core.js
@@ -29,5 +29,51 @@ var Cowl = {
 			instance[method]();
 		}
 		this.instances[command] = instance;
+	},
+	
+	/*
+		Method:
+			<Cowl.loadToPage>
+		
+		Loads a URL and with the JSON from results searches for elements with the attribute tpl-name and replaces the contents of them with it.
+		
+		Parameters:
+			(string) URL - The URL to load
+			(function) callback - Optional callback that is called when everything is done
+	*/
+	
+	loadToPage: function(url, callback) {
+		var req = new Request.JSON({
+			onSuccess: function(data) {
+				Cowl.templateReplace(data);
+				if ( typeof callback == 'function' )
+					callback();
+			}
+		});
+		req.get(url);
+	},
+	
+	/*
+		Method:
+			<Cowl.templateReplace>
+		
+		Replaces the contents of elements on the current page with the attribute tpl-name with their respective entry in data.
+		
+		Parameters:
+			(object) data - The data to find and replace
+	*/
+	
+	templateReplace: function(data) {
+		// Get elements with tpl-name properties
+		var elements = $$('*:tpl-name').each(function(element) {
+			var keys = element.get('tpl-name').split('.');
+			
+			var value = data[keys[0]], i = 1;
+			while ( typeof value != 'undefined' && keys[i] )
+				value = value[keys[i++]];
+			
+			if ( value )
+				element.set(element.get('tpl-attribute') || 'text', value);
+		});
 	}
 };
