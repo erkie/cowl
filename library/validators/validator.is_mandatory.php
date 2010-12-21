a991ab3feb8c4639dd8a5535a1e48b7ceb1fffeb
Erik Andersson <erik.rothoff@gmail.com>
2010-12-21 11:31:22 +0100

    Fixed bug in is_mandatory validator, it should now validate 0 correctly

--- library/validators/validator.is_mandatory.php
+++ library/validators/validator.is_mandatory.php
@@ -2,7 +2,9 @@
 
 function validate_is_mandatory($value, $arg)
 {
-	// if is_mandatory is false everything is A-OK
+	// if is_mandatory is false everything is valid
 	if ( ! $arg ) return true;
+	if ( $value === "0" || $value === 0 )
+		return true;
 	return ! empty($value);
 }
