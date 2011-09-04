38ac377dac2646b53ac585fa509fa3e565bb3e28
Erik Andersson <erik.rothoff@gmail.com>
2011-09-04 11:04:32 +0200

    p fix

--- library/helpers/helpers.standard.php
+++ library/helpers/helpers.standard.php
@@ -23,7 +23,12 @@ function p($str = '')
 	$args = func_get_args();
 	$args = array_slice($args, 1);
 	
-	echo htmlentities(vsprintf($str, $args));
+	if ( ! count($args) )
+		$html = $str;
+	else
+		$html = vsprintf($str);
+	
+	echo htmlentities($html);
 }
 
 /*
