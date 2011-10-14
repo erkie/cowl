849dcebd2fdc18c9c29ad0c1f12c116311e32629
Erik Andersson <erik.rothoff@gmail.com>
2011-10-14 10:20:31 +0200

    Updated social helper

--- library/helpers/helpers.social.php
+++ library/helpers/helpers.social.php
@@ -15,15 +15,19 @@
 	
 	Parameters:
 		(int) $limit - The number of links to limit to
+		(string) $url - The url to share
+		(string) $title - The title or text that is shared
+		(string) $short_title - A short title used where possible
 */
 
-function share_links($limit = 5, $url = '', $title = '')
+function share_links($limit = 5, $url = '', $title = '', $short_title = '')
 {
 	$url = urlencode($url);
 	$title = urlencode($title);
+	$short_title = urlencode($short_title);
 	
 	$db = array(
-		'facebook' => sprintf('http://www.facebook.com/sharer.php?u=%s&t=%s', $url, $title),
+		'facebook' => sprintf('https://www.facebook.com/sharer/sharer.php?s=100&p%%5Bsummary%%5D=%s&p%%5Burl%%5D=%s&p%%5Btitle%%5D=%s', $url, $title, $short_title),
 		'twitter' => sprintf('http://twitter.com/share?text=%s&url=%s', $title, $url),
 		'googleplus' => sprintf('https://m.google.com/app/plus/x/?v=compose&content=%s%%20%s', $url, $title),
 		'reddit' => sprintf('http://reddit.com/submit?url=%s&title=%s', $url, $title)
