885301888391cfd6243f35c9d272821bb7bc56a8
Erik Andersson <erik.rothoff@gmail.com>
2011-10-22 21:35:02 +0200

    Addedd share_url function to social helper

--- library/helpers/helpers.social.php
+++ library/helpers/helpers.social.php
@@ -20,7 +20,39 @@
 		(string) $short_title - A short title used where possible
 */
 
-function share_links($limit = 5, $url = '', $title = '', $short_title = '')
+function share_links($limit = 4, $url = '', $title = '', $short_title = '')
+{
+	$db = array('facebook', 'twitter', 'googleplus', 'reddit');
+	
+	$ret = array();
+	foreach ( $db as $key )
+	{
+		$ret[$key] = share_url($key, $url, $title, $short_title);
+	}
+	
+	return array_slice($ret, 0, $limit);
+}
+
+/*
+	Function:
+		share_url
+	
+	Get a url for a single social sharing site.
+	
+	Available sites:
+		facebook - Facebook.com
+		twitter - Twitter.com
+		googleplus - Google+
+		reddit - Reddit.com
+	
+	Parameters:
+		$name - The key of the site. Check "Available sites"
+		$url - The url to share
+		$title - The title or text that is shared
+		$short_title - A short title used where possible
+*/
+
+function share_url($name, $url, $title, $short_title)
 {
 	$url = urlencode($url);
 	$title = urlencode($title);
@@ -29,9 +61,9 @@ function share_links($limit = 5, $url = '', $title = '', $short_title = '')
 	$db = array(
 		'facebook' => sprintf('https://www.facebook.com/sharer/sharer.php?s=100&p%%5Bsummary%%5D=%s&p%%5Burl%%5D=%s&p%%5Btitle%%5D=%s', $title, $url, $short_title),
 		'twitter' => sprintf('http://twitter.com/share?text=%s&url=%s', $title, $url),
-		'googleplus' => sprintf('https://m.google.com/app/plus/x/?v=compose&content=%s%%20%s', $url, $title),
+		'googleplus' => sprintf('https://m.google.com/app/plus/x/?v=compose&hideloc=1&content=%s%%20%s', $url, $title),
 		'reddit' => sprintf('http://reddit.com/submit?url=%s&title=%s', $url, $title)
 	);
 	
-	return array_slice($db, 0, $limit);
+	return $db[$name];
 }
