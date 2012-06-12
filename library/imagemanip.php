5bb4af443acf918c739d3207b6eaa50de77f942f
Erik Andersson <erik.rothoff@gmail.com>
2012-06-13 00:39:30 +0200

    Completed the awesome image manipulation class with cropping, resizing, scaling and reformatting

--- library/imagemanip.php
+++ library/imagemanip.php
@@ -2,6 +2,7 @@
 
 class ImageManipFileNotFoundException extends Exception {}
 class ImageManipUnsupportedFormatException extends Exception {}
+class ImageManipBadGravity extends Exception {}
 class ImageManipException extends Exception {}
 
 class ImageManip
@@ -9,11 +10,23 @@ class ImageManip
 	public static $FORMATS = array('png', 'gif', 'jpg', 'jpeg');
 	
 	private $path;
+	
+	// Size settings
+	private $canvas_size = false;
 	private $size = false;
 	private $max_size = false;
+	
+	// Crop options
+	private $crop_gravity = array('center', 'center');
+	
+	// Save options
 	private $format = false;
 	
-	private $res = false;
+	// Internal
+	private $im = false; // The loaded image
+	private $res = false; // The loaded canvas
+	private $res_size = array(0, 0);
+	private $dest_pos = array(0, 0);
 	
 	public function __construct($path)
 	{
@@ -33,15 +46,49 @@ class ImageManip
 		$this->format = $format;
 	}
 	
+	/*
+		Method:
+			setCanvasSize
+		
+		Set the size of the image canvas. If present this will affect how <setSize> and <setMaxSize> are handled.
+		If present the image will be drawn onto a canvas $wx$h big.
+	*/
+	
+	public function setCanvasSize($w, $h)
+	{
+		$this->canvas_size = array($w, $h);
+	}
+	
+	/*
+		Method:
+			setSize
+		
+		Set the size of the image without taking into account any aspect ratio.
+	*/
+	
 	public function setSize($w, $h)
 	{
-		$this->size = array('w' => $w, 'h' => $h);
+		$this->size = array($w, $h);
 	}
+	
+	/*
+		Method:
+			setMaxSize
+		
+		Set the max size of an image. This will respect the aspect ratio of the image.
+	*/
 	
 	public function setMaxSize($w, $h)
 	{
-		$this->max_size = array('w' => $w, 'h' => $h);
+		$this->max_size = array($w, $h);
 	}
+	
+	/*
+		Method:
+			setFormat
+		
+		Force a specific image format.
+	*/
 	
 	public function setFormat($format)
 	{
@@ -53,6 +100,46 @@ class ImageManip
 		$this->format = $format;
 	}
 	
+	/*
+		Method:
+			setCropGravity
+		
+		Where the 'gravity' of a crop should be. So if an image is larger than the canvas, where should it 
+		base the crop off?
+		
+		It takes two values, one for the x-axis, and one for the y-axis.
+		
+		Possible values are:
+		
+			x: left, right, center
+			y: top, bottom, center
+		
+		Parameters:
+			$gravity_x - The x-axis
+			$gravity_y - The y-axis
+	*/
+	
+	public function setCropGravity($gravity_x, $gravity_y)
+	{
+		if ( ! in_array($gravity_x, array('left', 'right', 'center')) )
+			throw new ImageManipBadGravity($gravity_x);
+		
+		if ( ! in_array($gravity_y, array('top', 'center', 'bottom')) )
+			throw new ImageManipBadGravity($gravity_y);
+		
+		$this->crop_gravity = array($gravity_x, $gravity_y);
+	}
+	
+	/*
+		Method:
+			save
+		
+		Perform all cropping and processing and save the image.
+		
+		Parameters:
+			$filename - If specified, the output filename, otherwise this will overwrite the original image
+	*/
+	
 	public function save($filename = false)
 	{
 		$this->openImage();
@@ -60,10 +147,13 @@ class ImageManip
 		if ( $this->max_size )
 			$this->ensureMaxSize();
 		
-		if ( $this->size )
-			$this->ensureSize();
+		$this->makeCanvas();
+		$this->calculateCrop();
+		$this->perform();
 		
-		$this->saveToDisk($filename);
+		$this->saveToDisk($filename ?: $this->path);
+		
+		return true;
 	}
 	
 	private function openImage()
@@ -80,10 +170,13 @@ class ImageManip
 		);
 		
 		if ( ! isset($accepted_formats[$data['mime']]) )
-			throw new ImageManipException();
+			throw new ImageManipException('Bad mime');
 		
 		$load_function = sprintf('imagecreatefrom%s', $accepted_formats[$data['mime']]);
-		$this->res = call_user_func($load_function);
+		$this->im = call_user_func($load_function, $this->path);
+		
+		if ( ! $this->im )
+			throw new ImageManipException('Could not load');
 	}
 	
 	private function ensureMaxSize()
@@ -93,29 +186,80 @@ class ImageManip
 		$original_w = $w;
 		$original_h = $h;
 		
-		if ( $w > $this->max_size['w'] )
+		if ( $w > $this->max_size[0] )
 		{	
-			$h *= $this->max_size['w']/$w;
-			$w = $this->max_size['w'];
+			$h *= $this->max_size[0]/$w;
+			$w = $this->max_size[0];
 		}
 		
-		if ( $h > $this->max_size['h'] )
+		if ( $h > $this->max_size[1] )
 		{
-			$w *= $this->max_size['h']/$h;
-			$h = $this->max_size['h'];
+			$w *= $this->max_size[1]/$h;
+			$h = $this->max_size[1];
 		}
 		
 		$this->setSize($w, $h);
 	}
 	
-	private function ensureSize()
-	{
+	private function makeCanvas()
+	{	
+		// Get size of canvas
+		list($w, $h) = $this->canvas_size ?: $this->getSizeOnDisk();
 		
+		// Create GD canvas
+		$this->res = imagecreatetruecolor($w, $h);
+		$this->res_size = array($w, $h);
+	}
+	
+	private function calculateCrop()
+	{
+		list($canvas_w, $canvas_h) = $this->res_size;
+		
+		// First check if the destination size needs cropping
+		if ( $this->size && ($this->size[0] != $canvas_w || $this->size[1] != $canvas_h) )
+		{	
+			// Size is not the same as canvas, cropping will be had
+			list($w, $h) = $this->size;
+			
+			$cropmap = array(
+				'x' => array(
+					'left' => 0,
+					'center' => $canvas_w/2 - $w/2,
+					'right' => $canvas_w - $w
+				),
+				'y' => array(
+					'top' => 0,
+					'center' => $canvas_h/2 - $h/2,
+					'bottom' => $canvas_h - $h
+				)
+			);
+			
+			// This should calculate the x, y coordinates of the image to be drawn onto the canvas
+			$x = $cropmap['x'][$this->crop_gravity[0]];
+			$y = $cropmap['y'][$this->crop_gravity[1]];
+		}
+		else
+			list($x, $y) = array(0, 0);
+		
+		$this->dest_pos = array($x, $y);
+	}
+
+	private function perform()
+	{
+		list($w, $h) = $this->size ?: $this->res_size;
+		list($img_w, $img_h) = $this->getSizeOnDisk();
+		
+		imagecopyresampled($this->res, $this->im, $this->dest_pos[0], $this->dest_pos[1], 0, 0, $w, $h, $img_w, $img_h);
 	}
 	
 	private function saveToDisk($filename)
 	{
-		
+		if ( $this->format == 'jpg' )
+			imagejpeg($this->res, $filename, 100);
+		elseif ( $this->format == 'png' )
+			imagepng($this->res, $filename);
+		elseif ( $this->format == 'gif' )
+			imagegif($this->res, $filename);
 	}
 	
 	private function getSizeOnDisk()
